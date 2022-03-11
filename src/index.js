834d91aedb96ea2d7a22371af8412e370c2f127a
Andrey Alexeev <ipalexeev98@mail.ru>
2022-03-11 10:45:07 +0300

    edit readme.md

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./js/common.js";
+import "./js/customSelect.js"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/main.css";
 import "./assets/scss/main.scss";
--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,14 @@
-console.log('code from common js')
+console.log('code from common js');
+
+// slider settings
+
+$(document).ready(function(){
+  $('.slider-slick').slick({
+    arrows: true,
+    prevArrow: $('.controls__left'),
+    nextArrow: $('.controls__right'),
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false
+  });
+});
